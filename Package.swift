2d86c7ad8d1e0a07386b22e48b67018210e59a04
Bastian Veliz <bastianx6@gmail.com>
2020-09-12 19:01:12 -0300

    Update README.md

--- Package.swift
+++ Package.swift
@@ -10,19 +10,16 @@ let package = Package(
         .macOS(.v10_15)
     ],
     products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "AccountsCoreDataManagement",
             targets: ["AccountsCoreDataManagement"]),
     ],
     dependencies: [
-        .package(name: "DataManagement", url: "../accounts-data-management", from: "1.0.0"),
+        .package(name: "DataManagement", url: "https://github.com/bastianX6/accounts-data-management.git", from: "1.0.0"),
         .package(url: "https://github.com/Alexander-Ignition/CombineCoreData", from: "0.0.3"),
         .package(url: "https://github.com/apple/swift-log.git", from: "1.0.0"),
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "AccountsCoreDataManagement",
             dependencies: [
--- README.md
+++ README.md
@@ -1,3 +1,12 @@
 # AccountsCoreDataManagement
 
-A description of this package.
+## Overview
+
+This package implements the protocols from [DataManagement](https://github.com/bastianX6/accounts-data-management) package using `CoreData` as Data Source.
+
+- `CoreDataSourceModify` class implements the `DataSourceModify` protocol.
+- `CoreDataSourceRead` class implements the `DataSourceModify` protocol.
+
+## Persistence Controller
+
+The `PersistenceController` is a singleton that manages the `CoreData` persistent container initialization. It uses a `NSPersistentCloudKitContainer` instance.
