e25ec55b62807dbda2497632e0c35a04585f6e79
Bastian Veliz <bastianx6@gmail.com>
2020-09-15 19:59:26 -0300

    Update DataManagement dependency in Package.swift
    - Remove test target for watchOS

--- Package.swift
+++ Package.swift
@@ -3,11 +3,31 @@
 
 import PackageDescription
 
+private var targets: [Target] = [
+    .target(
+        name: "AccountsCoreDataManagement",
+        dependencies: [
+            "DataManagement",
+            "CombineCoreData",
+            .product(name: "Logging", package: "swift-log"),
+        ]),
+]
+
+#if os(iOS)
+    targets.append(
+        .testTarget(
+            name: "AccountsCoreDataManagementTests",
+            dependencies: ["AccountsCoreDataManagement"])
+    )
+#endif
+
 let package = Package(
     name: "AccountsCoreDataManagement",
     platforms: [
         .iOS(.v13),
-        .macOS(.v10_15)
+        .tvOS(.v13),
+        .macOS(.v10_15),
+        .watchOS(.v6),
     ],
     products: [
         .library(
@@ -15,20 +35,9 @@ let package = Package(
             targets: ["AccountsCoreDataManagement"]),
     ],
     dependencies: [
-        .package(name: "DataManagement", url: "https://github.com/bastianX6/accounts-data-management.git", from: "1.0.0"),
+        .package(name: "DataManagement", url: "https://github.com/bastianX6/accounts-data-management.git", from: "1.0.1"),
         .package(url: "https://github.com/Alexander-Ignition/CombineCoreData", from: "0.0.3"),
         .package(url: "https://github.com/apple/swift-log.git", from: "1.0.0"),
     ],
-    targets: [
-        .target(
-            name: "AccountsCoreDataManagement",
-            dependencies: [
-                "DataManagement",
-                "CombineCoreData",
-                .product(name: "Logging", package: "swift-log"),
-            ]),
-        .testTarget(
-            name: "AccountsCoreDataManagementTests",
-            dependencies: ["AccountsCoreDataManagement"]),
-    ]
+    targets: targets
 )
